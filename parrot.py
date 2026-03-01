7b22ac32833f5ccb01cf7a291bac3756322f9aa8
Ryma Djoudad <zenyod85@gmail.com>
2026-03-01 16:36:40 -0500

    added flag for active, added easter egg

--- parrot.py
+++ parrot.py
@@ -1,8 +1,13 @@
-prompt = "If you share your name, we can personalize the messages you see."
-prompt += "\n What is your first name? "
+prompt = "\nTell me something and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
 
-message = ''
-while message != 'quit':
+active = True
+while active:
     message = input(prompt)
-    if message != 'quit':
+    if message == 'quit':
+        active = False
+    elif message == 'parrot':
+        active = False
+        print("Oh, you think you're a wise guy, huh?")
+    else:
         print(message)
